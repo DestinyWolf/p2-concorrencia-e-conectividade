0190bb393ea88ff0ac906aacc250a575611ec789
Camila Boa Morte <116687830+camilaqPereira@users.noreply.github.com>
2024-11-02 15:48:10 -0300

    fix: merge conflict in mongo handler

--- Server/database/mongoHandler.py
+++ Server/database/mongoHandler.py
@@ -10,25 +10,40 @@ class MongoHandler:
         return self.client.get_collection(collection_name)
     
     def insert_data(self, data:dict, group:str):
-        self.__get_collection(group).insert_one(data)
+        try:
+            self.__get_collection(group).insert_one(data)
+        except  Exception as e:
+            print(f'[DATABASE] Fail to insert data, raise: {str(e)}')
+            raise
+
 
     def insert_many_data(self, list_of_data:list, group:str):
-        self.__get_collection(group).insert_many(list_of_data)
-
-    def  get_data_by_filter(self,filter:dict, group:str):
+        try:
+            self.__get_collection(group).insert_many(list_of_data)
+        except Exception as e:
+            print(f'[DATABASE] Fail to insert data, raise: {str(e)}')
+            raise
+        
+    def get_data_by_filter(self,filter:dict, group:str):
         response = self.__get_collection(group).find(filter)
 
         data = [item for item in response]
         
-        None if len(data)  == 0 else data
+        return None if len(data)  == 0 else data
         
     def get_all_itens_in_group(self,  group:str):
         response = self.__get_collection(group).find()
-        data = [item for  item in response]
+        data = [item for item in response]
 
-        None if len(data)  == 0 else data
+        return None if len(data)  == 0 else data
         
     def  delete_data_by_filter(self, filter:dict, group:str):
         response = self.__get_collection(group).delete_one(filter)
 
-        True if response.deleted_count > 0 else False
+        return True if response.deleted_count > 0 else False
+
+    def update_data_by_filter(self, group, filter, data):
+        response = self.__get_collection(group).replace_one(filter, data)
+
+        return True if response.matched_count > 0 else False
+
