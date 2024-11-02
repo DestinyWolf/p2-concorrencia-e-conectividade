2516ae93a84b9ed88b3b81d92e16cc4235bf9821
Camila Boa Morte <116687830+camilaqPereira@users.noreply.github.com>
2024-11-02 15:48:39 -0300

    refactor: enumerates for 2pc management

--- Server/utils/twoPhaseCommit.py
+++ Server/utils/twoPhaseCommit.py
@@ -2,13 +2,24 @@ from enum import Enum
 
 NODES_COUNT = 3
 
-class TransactionStates(Enum):
-    PREPARE = "prepare"
-    READY = "ready"
-    COMMIT = "commited"
-    ABORTED = "aborted"
+class TransactionStatus(Enum):
+    PREPARE = "PREPARE"
+    READY = "READY"
+    COMMIT = "COMMITED"
+    ABORTED = "ABORTED"
+    ILLEGAL = "ILLEGAL"
 
 class ServerIds(Enum):
     A = 0
     B = 1
     C = 2
+
+class ServerName(Enum):
+    A = 'Server-A'
+    B = 'Server-B'
+    C = 'Server-C'
+
+class ServerIP(Enum):
+    A = 'localhost1'
+    B = 'localhost2'
+    C = 'localhost3'
