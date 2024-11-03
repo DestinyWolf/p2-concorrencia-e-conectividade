b94a7c9abf4289eefbae125bfd3813f2b3e18006
Camila Boa Morte <116687830+camilaqPereira@users.noreply.github.com>
2024-11-03 03:08:46 -0300

    feat: transaction subclass for coordinator

---
+++ Server/TransactionProtocolState.py
@@ -0,0 +1,31 @@
+from Transaction import *
+
+class TransactionProtocolState(Transaction):
+    def __init__(self, coordinator_ip:ServerIds=None, transaction_id:str=None, participants=set(), intentions={}):
+        super().__init__(coordinator_ip, transaction_id, participants, intentions)
+        self.preparedToCommit = {}
+        self.done = {}
+
+    def load_transaction_from_db(self, transaction_id, db_handler):
+        restored_data = db_handler.get_data_by_filter({'_id': transaction_id}, CollectionsName.LOG.value)[0]
+        self.coordinator = restored_data['coordinator']
+        self.participants = restored_data['participants']
+        self.intentions = restored_data['intentions']
+        self.status = TransactionStatus(restored_data['status'])
+        self.preparedToCommit = restored_data['preparedToCommit']
+        self.done = restored_data['done']
+
+
+    def to_db_entry(self) -> dict:
+        return {'_id': self.transaction_id, 'coordinator': self.coordinator, 'participants': list(self.participants),
+                'intentions': self.intentions, 'status': self.status.value, 'preparedToCommit': self.preparedToCommit,
+                'done': self.done}
+
+        
+
+
+    
+
+        
+
+    
