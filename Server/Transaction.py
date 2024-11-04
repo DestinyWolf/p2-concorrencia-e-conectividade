9353b8fa37a832cae782b69294eae79891a697cb
Camila Boa Morte <116687830+camilaqPereira@users.noreply.github.com>
2024-11-04 00:08:24 -0300

    refactor: adding timestamp attribute

--- Server/Transaction.py
+++ Server/Transaction.py
@@ -1,14 +1,15 @@
 from utils.twoPhaseCommit import *
 from utils.database import *
-from database.mongoHandler import *
+from mongoHandler import *
 
 class Transaction():
-    def __init__(self, coordinator:str=None, transaction_id:str=None, participants=None, intentions=None):
+    def __init__(self, coordinator:str=None, transaction_id:str=None, participants=None, intentions=None, timestamp=None):
         self.coordinator:str = coordinator
         self.transaction_id:str = transaction_id
         self.participants:set = participants
         self.intentions = intentions #resources to use
         self.status:TransactionStatus = None
+        self.timestamp = timestamp
 
     def load_transaction_from_db(self, transaction_id, db_handler:MongoHandler):
         restored_data = db_handler.get_data_by_filter({'_id': transaction_id}, CollectionsName.LOG.value)[0]
@@ -17,6 +18,26 @@ class Transaction():
         self.participants = set(restored_data['participants'])
         self.intentions = restored_data['intentions']
         self.status = TransactionStatus(restored_data['status'])
+        self.timestamp = restored_data['timestamp']
     
     def to_db_entry(self) -> dict:
-        return {'_id': self.transaction_id, 'coordinator': self.coordinator, 'participants': list(self.participants), 'intentions': self.intentions, 'status': self.status.value}
+        return {'_id': self.transaction_id, 'coordinator': self.coordinator, 'participants': list(self.participants),
+                'intentions': self.intentions, 'status': self.status.value, 'timestamp': self.timestamp}
+
+
+    def __lt__(self, other):
+        self_bigger = False
+        peers_bigger = False
+    
+        for t_self, t_other in zip(self.timestamp, other.timestamp):
+            if t_self < t_other:
+                peers_bigger = True
+            elif t_self > t_other:
+                self_bigger = True
+        
+        if self_bigger and not peers_bigger:
+            return False #self is bigger
+        elif peers_bigger and not self_bigger:
+            return True #peer is bigger
+        else:
+            return True #concurrent
