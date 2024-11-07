aa006a9653fa6ae75194ad25a07ed84232164cba
Maike de Oliveira Nascimento <64764136+DestinyWolf@users.noreply.github.com>
2024-11-07 13:18:59 -0300

    Update twoPhaseCommit.py

--- Server/utils/twoPhaseCommit.py
+++ Server/utils/twoPhaseCommit.py
@@ -1,4 +1,5 @@
 from enum import Enum
+from utils.socketCommunicationProtocol import ConstantsManagement as cm
 
 NODES_COUNT = 3
 
@@ -19,6 +20,6 @@ class ServerName(Enum):
     B = 'Server-B'
     C = 'Server-C'
 
-SERVERIP:dict = {ServerName.A.value: '127.0.0.1', ServerName.B.value: '127.0.0.2', ServerName.C.value: '127.0.0.3'}
+SERVERIP:dict = {ServerName.A.value: cm.HOST.value, ServerName.B.value: '127.0.0.2', ServerName.C.value: '127.0.0.3'}
 
 SERVERPORT:dict = {ServerName.A.value: '5000', ServerName.B.value: '5001', ServerName.C.value: '5002'}
