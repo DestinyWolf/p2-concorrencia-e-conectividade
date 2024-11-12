9fbc7b17cbaf89b806cb8bb8f0b0fc2ff0e56535
Camila Boa Morte <116687830+camilaqPereira@users.noreply.github.com>
2024-11-12 14:23:03 -0300

    docs: adding docs for enums

--- Server/utils/twoPhaseCommit.py
+++ Server/utils/twoPhaseCommit.py
@@ -1,8 +1,14 @@
 from enum import Enum
 from utils.socketCommunicationProtocol import ConstantsManagement as cm
 
-NODES_COUNT = 3
+ 
+NODES_COUNT = 3 #Número de servidores no cluster
 
+
+##
+#   @brief: Classe utilizada para gerenciar os possiveis estados que uma transação pode assumir
+#   @note: estende a classe Enum
+##
 class TransactionStatus(Enum):
     PREPARE = "PREPARE"
     READY = "READY"
@@ -10,16 +16,27 @@ class TransactionStatus(Enum):
     ABORTED = "ABORTED"
     DONE = "DONE"
 
+##
+#   @brief: Classe utilizada para gerenciar os ids de cada servidor no cluster
+#   @note: estende a classe Enum
+##
 class ServerIds(Enum):
     A = 0
     B = 1
     C = 2
 
+##
+#   @brief: Classe utilizada para gerenciar os nomes de cada servidor no cluster
+#   @note: estende a classe Enum
+##
 class ServerName(Enum):
     A = 'Server-A'
     B = 'Server-B'
     C = 'Server-C'
 
+#Gerenciamento dos IPs de cada servidor no cluster
 SERVERIP:dict = {ServerName.A.value: '127.0.0.1', ServerName.B.value: '127.0.0.2', ServerName.C.value: '127.0.0.3'}
 
+
+#Gerenciamento das portas de cada servidor no cluster
 SERVERPORT:dict = {ServerName.A.value: '5000', ServerName.B.value: '5001', ServerName.C.value: '5002'}
