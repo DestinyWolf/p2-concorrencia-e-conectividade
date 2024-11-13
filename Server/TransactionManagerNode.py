10319a0731f84f834ea5c4b2c22da52a47aaaedf
Camila Boa Morte <116687830+camilaqPereira@users.noreply.github.com>
2024-11-12 21:12:06 -0300

    docs: adding doc for class and methods

--- Server/TransactionManagerNode.py
+++ Server/TransactionManagerNode.py
@@ -3,29 +3,40 @@ from TwoPhaseCommitNode import *
 from Transaction import *
 import requests
 
+
+##
+#   @brief: classe utilizada para gerenciar as requisições de compra recebidas de outros servidores
+##
 class TransactionManager(TwoPhaseCommitNode):
 
     def __init__(self, host_id, host_name, host_port=8000):
         super().__init__(host_id, host_name, host_port)
         self.logger.name = f"{type(self).__name__} - {self.host_name.value}"
             
-
+    ##
+    #   @brief: método utilizado adquirir os locks dos trechos de interesse da transação e verificar se 
+    #   a disponibilidade de assentos
+    #   @param: transaction: transação a ser executada
+    #   @return: decisão do servidor
+    ##
     def handle_prepare_RPC(self, transaction:Transaction) -> str:
+        #Verificando se a transação já foi executada
         recovered_transaction = self.db_handler.get_data_by_filter({'_id': transaction.transaction_id}, CollectionsName.LOG.value)
         
-        if recovered_transaction:
+        if recovered_transaction: #transação já executada anteriormente
             self.logger.warning(f"Transaction {transaction.transaction_id} already exists.")
             return recovered_transaction[0]['status']
-        else:
+        else: #nova transação
             #Write transaction to log
             transaction.status = TransactionStatus.PREPARE
             self.db_handler.insert_data(transaction.to_db_entry(), CollectionsName.LOG.value)
             self.logger.info(f"Transaction {transaction.transaction_id} accepted")
         
-            
+        #Adquirindo locks dos trechos de interesse
         for route in transaction.intentions: #locking routes
             self.graph.path_locks.get(route).acquire()
         
+        # Verificando disponibilidade dos trechos
         for route in transaction.intentions: #checking if there are sits available
             if self.graph.graph[route[0]][route[1]]['sits'] == 0:
                 transaction.status = TransactionStatus.ABORTED
@@ -34,31 +45,42 @@ class TransactionManager(TwoPhaseCommitNode):
         if transaction.status == TransactionStatus.PREPARE:
             transaction.status = TransactionStatus.READY 
         else:       
-            for route in transaction.intentions: #unlocking routes
+            for route in transaction.intentions: #liberando locks caso a operação seja abortada
                 self.graph.path_locks.get(route).release()
                     
         self.db_handler.update_data_by_filter(CollectionsName.LOG.value, {'_id':transaction.transaction_id}, transaction.to_db_entry())
         self.logger.info(f"{transaction.transaction_id} -> {transaction.status.value}")
         return transaction.status.value
 
+    ##
+    #   @brief: método utilizado para executar as alterações de commit
+    #   @param: recovered_transaction: transação a ser executada carregada do banco de dados
+    #   @return: retorna o status final da transação
+    ##
     def handle_commit_RPC(self, recovered_transaction:Transaction) -> str:
         
         
         if recovered_transaction.status == TransactionStatus.READY:                        
-        
+            #Atualizando status da transação
             recovered_transaction.status = TransactionStatus.COMMIT
             self.db_handler.update_data_by_filter(CollectionsName.LOG.value, {'_id': recovered_transaction.transaction_id}, recovered_transaction.to_db_entry())
             self.logger.info(f'Transaction {recovered_transaction.transaction_id} {recovered_transaction.status.value}')
 
             new_values = []
+            #Atualizando o numero de assentos das rotas de interesse
             for route in recovered_transaction.intentions:
                 u , v = route
                 self.graph.graph[u][v]['sits'] -= 1
                 
+                # Atualizando peso local
                 if self.graph.graph[u][v]['sits'] == 0:
                     self.graph.graph[u][v]['weight'] = 999
                     self.graph.graph[u][v]['company'][self.host_name.value] = 999
+
+                    #Atualizando peso global
                     self.graph.update_global_edge_weight((u,v))
+                    
+                    #Notificando peers sobre a indisponibilidade do assento
                     for peer, ip in SERVERIP.items():
                         if peer != self.host_name.value:
                             try:
@@ -73,7 +95,7 @@ class TransactionManager(TwoPhaseCommitNode):
                 self.graph.path_locks[(u, v)].release()
             
             self.db_handler.update_many(CollectionsName.GRAPH.value, new_values)
-        else:
+        else: #transação não está no status de READY
             self.logger.info(f"Illegal commit for {recovered_transaction.transaction_id}. Current state is {recovered_transaction.status}")
 
         recovered_transaction.status = TransactionStatus.DONE
@@ -82,6 +104,12 @@ class TransactionManager(TwoPhaseCommitNode):
         
         return recovered_transaction.status.value
     
+
+     ##
+    #   @brief: método utilizado para executar as alterações de abort
+    #   @param: recovered_transaction: transação a ser executada carregada do banco de dados
+    #   @return: retorna o status final da transação
+    ##
     def handle_abort_RPC(self, recovered_transaction: Transaction) -> str:
 
         if recovered_transaction.status == TransactionStatus.READY:
@@ -99,14 +127,3 @@ class TransactionManager(TwoPhaseCommitNode):
 
         return recovered_transaction.status.value
 
-
-
-'''
-tm = TransactionManager(ServerIds.A, ServerName.A)
-tm.handle_prepare_RPC(Transaction('Server-B', 'd85ca4b50ad9222414037776c90238f881890e878c94ad2d6aa6f353b81c0a', {'Server-A'},[('A','C'),('C','B')]))
-tm.handle_commit_RPC('d85ca4b50ad9222414037776c90238f881890e878c94ad2d6aa6f353b81c0a')
-res = tm.handle_abort_RPC('d85ca4b50ad9222414037776c90238f881890e878c94ad2d6aa6f353b81c0a')
-
-print(res)
-
-'''
